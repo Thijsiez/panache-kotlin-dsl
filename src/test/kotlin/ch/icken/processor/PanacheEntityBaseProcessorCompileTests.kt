1c0e4bc29e90684cd8d6df196907af21d09e9950
Thijs Koppen <thijs.koppen@gmail.com>
2025-03-16 21:37:44 +0100

    Update copyright for 2025

--- src/test/kotlin/ch/icken/processor/PanacheEntityBaseProcessorCompileTests.kt
+++ src/test/kotlin/ch/icken/processor/PanacheEntityBaseProcessorCompileTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Thijs Koppen
+ * Copyright 2024-2025 Thijs Koppen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
